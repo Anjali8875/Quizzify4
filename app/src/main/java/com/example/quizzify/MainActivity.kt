d4effcfcec05e3e61b41a39c57357d61064d64c4
Anjali8875 <anjali8875xic@gmail.com>
2023-12-23 19:52:04 +0530

    lots of changes done

--- app/src/main/java/com/example/quizzify/MainActivity.kt
+++ app/src/main/java/com/example/quizzify/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.quizzify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firebaseAuth=FirebaseAuth.getInstance()
+
+        val email=intent.getStringExtra("email")
+        val displayName=intent.getStringExtra("name")
     }
 }
